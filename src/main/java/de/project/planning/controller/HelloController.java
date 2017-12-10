e91e7b1822d0d998a617de655168d559b400ff35
Till <Till_Benecke@web.de>
2017-12-10 16:51:38 +0100

    Erste Verknüpfungen
    - Muss was in git.ignore gepackt werden?
    
    - Ich habe getestet wie man über den Dispatcher die jsp files über die url aufrufen kann. Das funktioniert auch. musst mal testen, aber an sich nix logisches nur bisschen grundding versucht.

--- src/main/java/de/project/planning/controller/HelloController.java
+++ src/main/java/de/project/planning/controller/HelloController.java
@@ -8,14 +8,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
-	
+
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String index(ModelMap model) {
-		
-		model.addAttribute("message", "Hello Spring WEB MVC!");
-	
-	return "hello";
-	}
-	
 
+		model.addAttribute("message", "Hello Spring WEB MVC!");
+
+		return "hello";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(ModelMap model) {
+
+		return "login";
+	}
+
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String test(ModelMap model) {
+
+		return "test";
+	}
+
+	
 }
